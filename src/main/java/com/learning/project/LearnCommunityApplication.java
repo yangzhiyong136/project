2a5a7c79eaa74eb761fd14d698ca5b43119d6eb3
yangzhiyong136 <1223576220@qq.com>
2019-07-30 18:20:45 +0800

    init repo

---
+++ src/main/java/com/learning/project/LearnCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.learning.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnCommunityApplication.class, args);
+    }
+
+}
