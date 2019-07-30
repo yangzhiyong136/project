8bd34bfd12be9ea40d842a933ac9df31d2bf87ef
yangzhiyong136 <1223576220@qq.com>
2019-07-30 23:34:20 +0800

    add index and bootst

--- src/main/java/com/learning/project/controller/HelloController.java
+++ src/main/java/com/learning/project/controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.learning.project.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Youngz
@@ -12,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
